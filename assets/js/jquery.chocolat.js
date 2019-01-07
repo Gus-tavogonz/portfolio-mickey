350df6e91a4b2c0a743b7a6a3eab13cf60838bf9
unknown <gustavogonz3393@gmail.com>
2019-01-06 19:41:12 -0500

    structure done

--- assets/js/jquery.chocolat.js
+++ assets/js/jquery.chocolat.js
@@ -1,4 +1,4 @@
-;(function(factory) {
+(function(factory) {
     if (typeof module === 'object' && typeof module.exports === 'object') {
         module.exports = factory(require('jquery'), window, document)
     } else {
